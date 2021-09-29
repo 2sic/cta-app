3e579274f0423ca090a92dd00f11a09753c8d641
Maxim Hüsler <maxim.huesler@2sic.com>
2021-09-29 17:04:48 +0200

    wip

---
+++ bs4/Helpers.cs
@@ -0,0 +1,15 @@
+using ToSic.Razor.Blade;
+
+public class Helpers: Custom.Hybrid.Code12
+{
+      public dynamic WrapperStyleAttribute(dynamic data) {
+            return (Text.Has(data.BackgroundImage) 
+                  ? "background-image: url('" + Link.Image(url: data.BackgroundImage, width: 1920, quality: 70) + "')"
+                  : "");
+      }
+}
+
+// <text>style='@(Text.Has(data.BackgroundImage) 
+//       ? "background-image: url('" + data.BackgroundImage + "?w=1920&amp;scale=both&amp;quality=70&amp;anchor=" + data.CropAnchor + "')"
+//       : "")'</text>
+// }
